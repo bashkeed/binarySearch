6ae3165cc2fc99ce1f5ce5a306afa93ba21458d1
zubair <bashirkwarra@gmail.com>
2024-08-01 12:47:41 +0100

    initial Commit

---
+++ searchList.js
@@ -0,0 +1,25 @@
+const search = array =>{
+    let right = array.length -1;
+    let left = 0;
+    let mid ;
+    let x = 6
+
+    while(left < right){
+        mid = left +(right - left)/2;
+
+        if(array[mid]===x)
+            return mid
+
+        if(array[mid]< x){
+            left = mid +1
+        }else{
+            right = mid -1
+        }
+         
+    }
+    return -1 
+    
+}
+const arr =[3,5,6,9,11,18,20,21,24,30]
+
+console.log(search(arr))
